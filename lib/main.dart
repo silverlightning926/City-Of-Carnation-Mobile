9008c40bb8760108beaf7ea080686b91dc9019d3
Sidd Rao <siddrao.926@gmail.com>
2023-04-28 14:16:14 -0700

    Added Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'package:city_of_carnation/screens/login_screen.dart';
 import 'package:city_of_carnation/screens/welcome_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(const CityOfCarnation());
 }
 
