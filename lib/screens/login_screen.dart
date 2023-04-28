25d0cbcdb6374a46856be4ce8b9ee847ae84cd07
Sidd Rao <siddrao.926@gmail.com>
2023-04-28 14:46:04 -0700

    Edited Login Screen UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -12,15 +12,24 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Login"),
-      ),
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(),
       body: Padding(
         padding: const EdgeInsets.all(20),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
+            SvgPicture.asset(
+              './assets/images/login_screen_image.svg',
+              width: 200,
+            ),
+            Text(
+              "Login",
+              style: Theme.of(context).textTheme.headline2?.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
             Column(
               children: const [
                 TextField(
