c6211386bb263413cdeac52f833ec8e5e2bfeb33
Sidd Rao <siddrao.926@gmail.com>
2023-05-03 16:46:03 -0700

    Added To App Style

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:city_of_carnation/screens/home_screen.dart';
 import 'package:city_of_carnation/screens/login_screen.dart';
 import 'package:city_of_carnation/screens/signup_screen.dart';
 import 'package:city_of_carnation/screens/welcome_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -23,12 +25,22 @@ class CityOfCarnation extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'City Of Carnation',
-      theme: ThemeData.dark().copyWith(),
-      initialRoute: '/',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          color: Colors.black,
+          titleTextStyle: Theme.of(context).textTheme.headline4!.copyWith(
+                color: Colors.white,
+              ),
+        ),
+      ),
+      initialRoute:
+          FirebaseAuth.instance.currentUser == null ? '/welcome' : '/home ',
       routes: {
-        '/': (context) => const WelcomeScreen(),
+        '/welcome': (context) => const WelcomeScreen(),
         '/login': (context) => const LoginScreen(),
         '/signup': (context) => const SignupScreen(),
+        '/home': (context) => const HomeScreen(),
       },
     );
   }
