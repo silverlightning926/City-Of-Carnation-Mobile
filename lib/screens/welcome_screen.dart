0ccd894a068966224749cb843a52f7774ed43daf
Sidd Rao <siddrao.926@gmail.com>
2023-05-03 16:45:27 -0700

    Cleaned Up Welcome Screen UI

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -13,93 +13,102 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SvgPicture.asset(
-              './assets/images/welcome_screen_image.svg',
-              width: 300,
-            ),
-            Column(
-              children: [
-                Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: Text(
-                        'City of Carnation',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.headline1!.copyWith(
-                              color: Colors.white,
-                              fontSize: 40,
-                              fontWeight: FontWeight.w700,
-                            ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        vertical: 5.0,
-                        horizontal: 20.0,
-                      ),
-                      child: Text(
-                        'Your Natural Destination in the Valleys of Washington!',
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                              color: Colors.grey[400],
-                              fontWeight: FontWeight.w400,
-                            ),
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 13.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
+        resizeToAvoidBottomInset: false,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(
+            vertical: 60,
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SvgPicture.asset(
+                './assets/images/welcome_screen_image.svg',
+                width: 300,
+              ),
+              Column(
+                children: [
+                  Column(
                     children: [
-                      OutlinedButton(
-                        style: ElevatedButton.styleFrom(
-                          minimumSize: const Size(100, 75),
-                          foregroundColor: Colors.white,
-                          backgroundColor: const Color(0xFF6C63FF),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: Text(
+                          'City of Carnation',
+                          textAlign: TextAlign.center,
+                          style:
+                              Theme.of(context).textTheme.headline1!.copyWith(
+                                    color: Colors.white,
+                                    fontSize: 40,
+                                    fontWeight: FontWeight.w700,
+                                  ),
                         ),
-                        onPressed: () => Navigator.pushNamed(context, '/login'),
-                        child: const Text('Login'),
                       ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      OutlinedButton(
-                        style: OutlinedButton.styleFrom(
-                          minimumSize: const Size(100, 75),
-                          foregroundColor: Colors.white,
-                          backgroundColor:
-                              const Color.fromARGB(50, 107, 99, 255),
-                          shape: const RoundedRectangleBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(10),
-                            ),
-                          ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 5.0,
+                          horizontal: 20.0,
                         ),
-                        onPressed: () =>
-                            Navigator.pushNamed(context, '/signup'),
-                        child: const Text('Register'),
+                        child: Text(
+                          'Your Natural Destination in the Valleys of Washington!',
+                          textAlign: TextAlign.center,
+                          style:
+                              Theme.of(context).textTheme.bodyText1!.copyWith(
+                                    color: Colors.grey[400],
+                                    fontWeight: FontWeight.w400,
+                                  ),
+                        ),
                       ),
                     ],
                   ),
-                ),
-              ],
-            ),
-          ],
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 13.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        OutlinedButton(
+                          style: ElevatedButton.styleFrom(
+                            minimumSize: const Size(100, 75),
+                            foregroundColor: Colors.white,
+                            backgroundColor: const Color(0xFF6C63FF),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          onPressed: () =>
+                              Navigator.pushNamed(context, '/login'),
+                          child: const Text('Login'),
+                        ),
+                        const SizedBox(
+                          height: 10,
+                        ),
+                        OutlinedButton(
+                          style: OutlinedButton.styleFrom(
+                            minimumSize: const Size(100, 75),
+                            foregroundColor: Colors.white,
+                            backgroundColor:
+                                const Color.fromARGB(50, 107, 99, 255),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.all(
+                                Radius.circular(10),
+                              ),
+                            ),
+                          ),
+                          onPressed: () =>
+                              Navigator.pushNamed(context, '/signup'),
+                          child: const Text('Register'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
