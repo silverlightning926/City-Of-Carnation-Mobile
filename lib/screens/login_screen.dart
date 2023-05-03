2df0eef14a132bfc36be15766dd9499d3e4d2cf3
Sidd Rao <siddrao.926@gmail.com>
2023-05-03 16:46:20 -0700

    Edited Login Screen UI

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -9,67 +10,123 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            SvgPicture.asset(
-              './assets/images/login_screen_image.svg',
-              width: 200,
-            ),
-            Text(
-              "Login",
-              style: Theme.of(context).textTheme.headline2?.copyWith(
-                    color: Colors.white,
+    return SafeArea(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              SvgPicture.asset(
+                './assets/images/login_screen_image.svg',
+                width: 200,
+              ),
+              Text(
+                "Login",
+                style: Theme.of(context).textTheme.headline2!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+              Column(
+                children: [
+                  TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-            ),
-            Column(
-              children: const [
-                TextField(
-                  decoration: InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+              Visibility(
+                visible: false,
+                maintainState: true,
+                maintainSize: true,
+                maintainAnimation: true,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(70, 244, 67, 54),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Error Message',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
                   ),
                 ),
-                SizedBox(
-                  height: 25,
-                ),
-                TextField(
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      // TODO: Show Loading Indicator
+                      try {
+                        FirebaseAuth.instance
+                            .signInWithEmailAndPassword(
+                              email: _emailController.text,
+                              password: _passwordController.text,
+                            )
+                            .then((value) => {
+                                  Navigator.pushNamed(context, '/home'),
+                                });
+                      } on FirebaseAuthException catch (exception) {
+                        // TODO: Handle FirebaseAuthException With Error Message
+                        if (exception.code == 'user-not-found') {
+                          print('No user found for that email.');
+                        } else if (exception.code == 'wrong-password') {
+                          print('Wrong password provided for that user.');
+                        }
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 75),
+                      backgroundColor: const Color(0xFF6C63FF),
+                    ),
+                    child: const Text('Login'),
                   ),
-                ),
-              ],
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(100, 75),
-                    backgroundColor: const Color(0xFF6C63FF),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('Forgot Password?'),
                   ),
-                  child: const Text('Login'),
-                ),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Forgot Password?'),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
