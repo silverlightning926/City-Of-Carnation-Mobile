56579478b9eb742cc25dbc77569592664158daea
Sidd Rao <siddrao.926@gmail.com>
2023-05-12 16:58:55 -0700

    Added Functionality To Read Posts and Events From Firestore

--- lib/managers/firestore_manager.dart
+++ lib/managers/firestore_manager.dart
@@ -1,3 +1,4 @@
+import 'package:city_of_carnation/serialized/event.dart';
 import 'package:city_of_carnation/serialized/post.dart';
 import 'package:city_of_carnation/serialized/user_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -15,19 +16,29 @@ class FireStoreManager {
       "phone": phone,
     };
 
-    return FirebaseFirestore.instance.collection("userInfo").doc(uid).set(user);
+    return FirebaseFirestore.instance
+        .collection("user-info")
+        .doc(uid)
+        .set(user);
   }
 
   static Future<UserData> getUserData(String uid) async {
     final DocumentSnapshot<Map<String, dynamic>> user =
-        await FirebaseFirestore.instance.collection("userInfo").doc(uid).get();
+        await FirebaseFirestore.instance.collection("user-info").doc(uid).get();
     return UserData.fromJson(user.data()!);
   }
 
   static Future<List<Post>> getPostData() async {
-    final QuerySnapshot<Map<String, dynamic>> post =
+    final QuerySnapshot<Map<String, dynamic>> posts =
         await FirebaseFirestore.instance.collection("post-feed").get();
 
-    return post.docs.map((e) => Post.fromJson(e.data())).toList();
+    return posts.docs.map((e) => Post.fromJson(e.data())).toList();
+  }
+
+  static Future<List<Event>> getEventData() async {
+    final QuerySnapshot<Map<String, dynamic>> events =
+        await FirebaseFirestore.instance.collection("event-info").get();
+
+    return events.docs.map((e) => Event.fromJson(e.data())).toList();
   }
 }
