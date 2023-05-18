64874c4cf3ff6efe837165141bcd76cbd9ac5b0a
Sidd Rao <siddrao.926@gmail.com>
2023-05-17 17:32:07 -0700

    Rudimentary Feed Tab

---
+++ lib/screens/tabs/feed_tab.dart
@@ -0,0 +1,37 @@
+import 'package:city_of_carnation/components/news_card.dart';
+import 'package:city_of_carnation/serialized/post.dart';
+import 'package:flutter/material.dart';
+
+class FeedTab extends StatelessWidget {
+  const FeedTab({
+    Key? key,
+    required this.posts,
+  }) : super(key: key);
+
+  final List<Post> posts;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Text(
+            'Feed',
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  color: Colors.white,
+                ),
+          ),
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: posts.length,
+              itemBuilder: ((context, index) => NewsCard(post: posts[index])),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
