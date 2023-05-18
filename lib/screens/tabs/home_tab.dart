9e1d0611691f135e0b67abfa00de031289586a15
Sidd Rao <siddrao.926@gmail.com>
2023-05-17 17:31:50 -0700

    Changed Home Screen Event Cards To Mini Event Cards

--- lib/screens/tabs/home_tab.dart
+++ lib/screens/tabs/home_tab.dart
@@ -1,4 +1,4 @@
-import 'package:city_of_carnation/components/event_card.dart';
+import 'package:city_of_carnation/components/mini_event_card.dart';
 import 'package:city_of_carnation/components/news_card.dart';
 import 'package:city_of_carnation/serialized/event.dart';
 import 'package:city_of_carnation/serialized/post.dart';
@@ -9,69 +9,80 @@ class HomeTab extends StatelessWidget {
     Key? key,
     required this.featuredPost,
     required this.upcomingEvents,
+    required this.name,
   }) : super(key: key);
 
+  final String name;
   final Post featuredPost;
   final List<Event> upcomingEvents;
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: ListView(
-        padding: const EdgeInsets.only(
-          left: 20,
-          right: 20,
-          bottom: 20,
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          Expanded(
-            child: Container(
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.grey[900],
-              ),
-              child: const Text(
-                'Welcome to the City Of Carnation mobile app! We want to make it easier for you to connect with your city. We hope you enjoy the app and find it useful. If you have any questions or feedback, please email us at cityhall@carnationwa.gov.',
-                style: TextStyle(
+          Text(
+            'Hello $name!',
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
                   color: Colors.white,
-                  fontSize: 16,
                 ),
-              ),
-            ),
           ),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 10.0),
-            child: Text(
-              'Featured News',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
-          NewsCard(post: featuredPost),
-          const Padding(
-            padding: EdgeInsets.symmetric(vertical: 10.0),
-            child: Text(
-              'Upcoming Events',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 250,
-            child: ListView.builder(
-                itemBuilder: (context, index) => EventCard(
-                      event: upcomingEvents[index],
+          const SizedBox(height: 10),
+          Expanded(
+            child: ListView(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.grey[900],
+                  ),
+                  child: const Text(
+                    'Welcome to the City Of Carnation mobile app! We want to make it easier for you to connect with your city. We hope you enjoy the app and find it useful. If you have any questions or feedback, please email us at cityhall@carnationwa.gov.',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
                     ),
-                itemCount: upcomingEvents.length,
-                scrollDirection: Axis.horizontal),
-          ),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    'Featured News',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                NewsCard(post: featuredPost),
+                const Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10.0),
+                  child: Text(
+                    'Upcoming Events',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 250,
+                  child: ListView.builder(
+                      itemBuilder: (context, index) => MiniEventCard(
+                            event: upcomingEvents[index],
+                          ),
+                      itemCount: upcomingEvents.length,
+                      scrollDirection: Axis.horizontal),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
