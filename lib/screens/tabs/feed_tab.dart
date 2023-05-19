6d92628a1e4590981f8265769d3228ea554726f4
Sidd Rao <siddrao.926@gmail.com>
2023-05-19 13:51:03 -0700

    Changed Spacing On Feed Tab

--- lib/screens/tabs/feed_tab.dart
+++ lib/screens/tabs/feed_tab.dart
@@ -23,7 +23,7 @@ class FeedTab extends StatelessWidget {
                   color: Colors.white,
                 ),
           ),
-          const SizedBox(height: 10),
+          const SizedBox(height: 20),
           Expanded(
             child: ListView.builder(
               itemCount: posts.length,
