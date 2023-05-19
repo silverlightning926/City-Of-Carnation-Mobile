58f5caf3e87ba9b3cc859dca41f25dadc766cd2d
Sidd Rao <siddrao.926@gmail.com>
2023-05-19 14:21:05 -0700

    UI Changes To Event Screen

--- lib/screens/event_screen.dart
+++ lib/screens/event_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:city_of_carnation/serialized/event.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
@@ -92,6 +93,122 @@ class EventScreen extends StatelessWidget {
                                     fontStyle: FontStyle.italic,
                                   ),
                             ),
+                            const SizedBox(height: 15),
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                height: 250,
+                                child: FlutterMap(
+                                  mapController: mapController,
+                                  options: MapOptions(
+                                    center: LatLng(
+                                      event.location!.latitude,
+                                      event.location!.longitude,
+                                    ),
+                                    maxBounds: LatLngBounds(
+                                      LatLng(
+                                        event.location!.latitude - 0.05,
+                                        event.location!.longitude - 0.05,
+                                      ),
+                                      LatLng(
+                                        event.location!.latitude + 0.05,
+                                        event.location!.longitude + 0.05,
+                                      ),
+                                    ),
+                                    minZoom: 14.0,
+                                    maxZoom: 18.0,
+                                    zoom: 14.0,
+                                    interactiveFlags:
+                                        InteractiveFlag.pinchZoom |
+                                            InteractiveFlag.drag,
+                                  ),
+                                  nonRotatedChildren: [
+                                    Align(
+                                      alignment: Alignment.topRight,
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Container(
+                                          decoration: BoxDecoration(
+                                            color:
+                                                Colors.black.withOpacity(0.7),
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                          ),
+                                          child: Row(
+                                            mainAxisSize: MainAxisSize.min,
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.spaceEvenly,
+                                            children: [
+                                              IconButton(
+                                                padding: EdgeInsets.zero,
+                                                onPressed: () {
+                                                  mapController.move(
+                                                    LatLng(
+                                                      event.location!.latitude,
+                                                      event.location!.longitude,
+                                                    ),
+                                                    14.0,
+                                                  );
+                                                },
+                                                icon: const Icon(
+                                                  Icons.my_location,
+                                                  color: Colors.white,
+                                                  size: 22,
+                                                ),
+                                              ),
+                                              IconButton(
+                                                padding: EdgeInsets.zero,
+                                                onPressed: () {
+                                                  MapsLauncher.launchCoordinates(
+                                                      event.location!.latitude,
+                                                      event.location!.longitude,
+                                                      '${event.locationName}');
+                                                },
+                                                icon: const Icon(
+                                                  Icons.directions,
+                                                  color: Colors.white,
+                                                  size: 22,
+                                                ),
+                                              ),
+                                            ],
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                  children: [
+                                    TileLayer(
+                                      urlTemplate:
+                                          'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+                                      userAgentPackageName:
+                                          'com.carnationwa.cityofcarnation',
+                                    ),
+                                    MarkerLayer(
+                                      markers: [
+                                        Marker(
+                                          point: LatLng(
+                                            event.location!.latitude,
+                                            event.location!.longitude,
+                                          ),
+                                          width: 80,
+                                          height: 80,
+                                          builder: (context) => const Padding(
+                                            padding:
+                                                EdgeInsets.only(bottom: 50.0),
+                                            child: Icon(
+                                              Icons.location_on,
+                                              color: Colors.red,
+                                              size: 50,
+                                            ),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ),
                           ],
                         ),
                       ),
@@ -110,138 +227,6 @@ class EventScreen extends StatelessWidget {
                                     ),
                           ),
                         ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          height: 250,
-                          child: FlutterMap(
-                            mapController: mapController,
-                            options: MapOptions(
-                              center: LatLng(
-                                event.location!.latitude,
-                                event.location!.longitude,
-                              ),
-                              maxBounds: LatLngBounds(
-                                LatLng(
-                                  event.location!.latitude - 0.05,
-                                  event.location!.longitude - 0.05,
-                                ),
-                                LatLng(
-                                  event.location!.latitude + 0.05,
-                                  event.location!.longitude + 0.05,
-                                ),
-                              ),
-                              minZoom: 14.0,
-                              maxZoom: 18.0,
-                              zoom: 14.0,
-                              interactiveFlags: InteractiveFlag.pinchZoom |
-                                  InteractiveFlag.drag,
-                            ),
-                            nonRotatedChildren: [
-                              Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    color: Colors.black.withOpacity(0.7),
-                                    borderRadius: BorderRadius.circular(10),
-                                  ),
-                                  padding: const EdgeInsets.symmetric(
-                                    horizontal: 15.0,
-                                    vertical: 5.0,
-                                  ),
-                                  child: Row(
-                                    mainAxisAlignment:
-                                        MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment:
-                                        CrossAxisAlignment.center,
-                                    children: [
-                                      Text(
-                                        '${event.locationName}',
-                                        style: Theme.of(context)
-                                            .textTheme
-                                            .bodyText1!
-                                            .copyWith(
-                                              fontSize: 20,
-                                              height: 1.3,
-                                            ),
-                                      ),
-                                      Row(
-                                        crossAxisAlignment:
-                                            CrossAxisAlignment.center,
-                                        children: [
-                                          IconButton(
-                                            padding: EdgeInsets.zero,
-                                            onPressed: () {
-                                              mapController.move(
-                                                LatLng(
-                                                  event.location!.latitude,
-                                                  event.location!.longitude,
-                                                ),
-                                                14.0,
-                                              );
-                                            },
-                                            icon: const Icon(
-                                              Icons.my_location,
-                                              color: Colors.white,
-                                              size: 30,
-                                            ),
-                                          ),
-                                          IconButton(
-                                            padding: EdgeInsets.zero,
-                                            onPressed: () {
-                                              MapsLauncher.launchCoordinates(
-                                                  event.location!.latitude,
-                                                  event.location!.longitude,
-                                                  '${event.locationName}');
-                                            },
-                                            icon: const Icon(
-                                              Icons.directions,
-                                              color: Colors.white,
-                                              size: 30,
-                                            ),
-                                          ),
-                                        ],
-                                      )
-                                    ],
-                                  ),
-                                ),
-                              ),
-                            ],
-                            children: [
-                              TileLayer(
-                                urlTemplate:
-                                    'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                                userAgentPackageName:
-                                    'com.carnationwa.cityofcarnation',
-                              ),
-                              MarkerLayer(
-                                markers: [
-                                  Marker(
-                                    point: LatLng(
-                                      event.location!.latitude,
-                                      event.location!.longitude,
-                                    ),
-                                    width: 80,
-                                    height: 80,
-                                    builder: (context) => const Padding(
-                                      padding: EdgeInsets.only(bottom: 50.0),
-                                      child: Icon(
-                                        Icons.location_on,
-                                        color: Colors.red,
-                                        size: 50,
-                                      ),
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ],
-                          ),
-                        ),
-                      ),
                     ],
                   ),
                 );
