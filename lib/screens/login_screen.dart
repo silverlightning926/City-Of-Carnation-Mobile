7981073f55799131c8b657c5da0bc375e0ecfe34
Sidd Rao <siddrao.926@gmail.com>
2023-05-22 15:25:11 -0700

    Added Loading Overlay To Login Screen

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -25,120 +26,125 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
-        appBar: AppBar(),
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              SvgPicture.asset(
-                './assets/images/login_screen_image.svg',
-                width: 200,
-              ),
-              Text(
-                "Login",
-                style: Theme.of(context).textTheme.headline2!.copyWith(
-                      color: Colors.white,
+      child: LoaderOverlay(
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
+          appBar: AppBar(),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SvgPicture.asset(
+                  './assets/images/login_screen_image.svg',
+                  width: 200,
+                ),
+                Text(
+                  "Login",
+                  style: Theme.of(context).textTheme.headline2!.copyWith(
+                        color: Colors.white,
+                      ),
+                ),
+                Column(
+                  children: [
+                    TextField(
+                      controller: _emailController,
+                      decoration: const InputDecoration(
+                        labelText: 'Email',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
-              ),
-              Column(
-                children: [
-                  TextField(
-                    controller: _emailController,
-                    decoration: const InputDecoration(
-                      labelText: 'Email',
-                      border: OutlineInputBorder(),
+                    const SizedBox(
+                      height: 25,
                     ),
-                  ),
-                  const SizedBox(
-                    height: 25,
-                  ),
-                  TextField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    enableSuggestions: false,
-                    autocorrect: false,
-                    decoration: const InputDecoration(
-                      labelText: 'Password',
-                      border: OutlineInputBorder(),
+                    TextField(
+                      controller: _passwordController,
+                      obscureText: true,
+                      enableSuggestions: false,
+                      autocorrect: false,
+                      decoration: const InputDecoration(
+                        labelText: 'Password',
+                        border: OutlineInputBorder(),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-              Visibility(
-                visible: false,
-                maintainState: true,
-                maintainSize: true,
-                maintainAnimation: true,
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: const Color.fromARGB(70, 244, 67, 54),
-                  ),
-                  padding: const EdgeInsets.all(10.0),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      'Error Message',
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                            color: Colors.white,
-                          ),
+                  ],
+                ),
+                Visibility(
+                  visible: false,
+                  maintainState: true,
+                  maintainSize: true,
+                  maintainAnimation: true,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: const Color.fromARGB(70, 244, 67, 54),
+                    ),
+                    padding: const EdgeInsets.all(10.0),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        'Error Message',
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                              color: Colors.white,
+                            ),
+                      ),
                     ),
                   ),
                 ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // TODO: Show Loading Indicator
-                      try {
-                        FirebaseAuth.instance
-                            .signInWithEmailAndPassword(
-                              email: _emailController.text,
-                              password: _passwordController.text,
-                            )
-                            .then((value) => {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) =>
-                                          const LoadingScreen(),
-                                      settings: const RouteSettings(
-                                        name: 'LoadingScreen',
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        context.loaderOverlay.show();
+                        try {
+                          FirebaseAuth.instance
+                              .signInWithEmailAndPassword(
+                                email: _emailController.text,
+                                password: _passwordController.text,
+                              )
+                              .then((value) => {
+                                    Navigator.push(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) =>
+                                            const LoadingScreen(),
+                                        settings: const RouteSettings(
+                                          name: 'LoadingScreen',
+                                        ),
                                       ),
+                                    ).then(
+                                      (value) => context.loaderOverlay.hide(),
                                     ),
-                                  ),
-                                  FirebaseAnalytics.instance
-                                      .logLogin(loginMethod: 'email'),
-                                });
-                      } on FirebaseAuthException catch (exception) {
-                        // TODO: Handle FirebaseAuthException With Error Message
-                        if (exception.code == 'user-not-found') {
-                          print('No user found for that email.');
-                        } else if (exception.code == 'wrong-password') {
-                          print('Wrong password provided for that user.');
+                                    FirebaseAnalytics.instance
+                                        .logLogin(loginMethod: 'email'),
+                                  });
+                        } on FirebaseAuthException catch (exception) {
+                          context.loaderOverlay.hide();
+                          // TODO: Handle FirebaseAuthException With Error Message
+                          if (exception.code == 'user-not-found') {
+                            print('No user found for that email.');
+                          } else if (exception.code == 'wrong-password') {
+                            print('Wrong password provided for that user.');
+                          }
                         }
-                      }
-                    },
-                    style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(100, 75),
-                      backgroundColor: const Color(0xFF6C63FF),
+                      },
+                      style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(100, 75),
+                        backgroundColor: const Color(0xFF6C63FF),
+                      ),
+                      child: const Text('Login'),
                     ),
-                    child: const Text('Login'),
-                  ),
-                  TextButton(
-                    onPressed: () {},
-                    child: const Text('Forgot Password?'),
-                  ),
-                ],
-              ),
-            ],
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text('Forgot Password?'),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
