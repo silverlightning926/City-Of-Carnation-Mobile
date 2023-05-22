9f3a31dac095c533e260698160d8fa404c38897b
Sidd Rao <siddrao.926@gmail.com>
2023-05-22 15:24:58 -0700

    Added Loading Overlay To Sign Up Screen

--- lib/screens/signup_screen.dart
+++ lib/screens/signup_screen.dart
@@ -4,6 +4,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:loader_overlay/loader_overlay.dart';
 
 class SignupScreen extends StatefulWidget {
   const SignupScreen({super.key});
@@ -32,151 +33,156 @@ class _SignupScreenState extends State<SignupScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              "Register",
-              style: Theme.of(context).textTheme.headline2!.copyWith(
-                    color: Colors.white,
+    return LoaderOverlay(
+      child: Scaffold(
+        resizeToAvoidBottomInset: false,
+        appBar: AppBar(),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Text(
+                "Register",
+                style: Theme.of(context).textTheme.headline2!.copyWith(
+                      color: Colors.white,
+                    ),
+              ),
+              Column(
+                children: [
+                  TextField(
+                    controller: _nameController,
+                    decoration: const InputDecoration(
+                      labelText: 'Name',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-            ),
-            Column(
-              children: [
-                TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(
-                    labelText: 'Name',
-                    border: OutlineInputBorder(),
+                  const SizedBox(
+                    height: 25,
                   ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                TextField(
-                  controller: _emailController,
-                  decoration: const InputDecoration(
-                    labelText: 'Email',
-                    border: OutlineInputBorder(),
+                  TextField(
+                    controller: _emailController,
+                    decoration: const InputDecoration(
+                      labelText: 'Email',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                TextField(
-                  controller: _phoneController,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    labelText: 'Phone',
-                    border: OutlineInputBorder(),
+                  const SizedBox(
+                    height: 25,
                   ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                TextField(
-                  controller: _passwordController,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+                  TextField(
+                    controller: _phoneController,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      labelText: 'Phone',
+                      border: OutlineInputBorder(),
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  height: 25,
-                ),
-                TextField(
-                  controller: _confirmPasswordController,
-                  obscureText: true,
-                  enableSuggestions: false,
-                  autocorrect: false,
-                  decoration: const InputDecoration(
-                    labelText: 'Confirm Password',
-                    border: OutlineInputBorder(),
+                  const SizedBox(
+                    height: 25,
                   ),
-                ),
-              ],
-            ),
-            Visibility(
-              visible: false,
-              maintainState: true,
-              maintainSize: true,
-              maintainAnimation: true,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10),
-                  color: const Color.fromARGB(70, 244, 67, 54),
-                ),
-                padding: const EdgeInsets.all(10.0),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Text(
-                    'Error Message',
-                    textAlign: TextAlign.center,
-                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
-                          color: Colors.white,
-                        ),
+                  TextField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  TextField(
+                    controller: _confirmPasswordController,
+                    obscureText: true,
+                    enableSuggestions: false,
+                    autocorrect: false,
+                    decoration: const InputDecoration(
+                      labelText: 'Confirm Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ],
+              ),
+              Visibility(
+                visible: false,
+                maintainState: true,
+                maintainSize: true,
+                maintainAnimation: true,
+                child: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color.fromARGB(70, 244, 67, 54),
+                  ),
+                  padding: const EdgeInsets.all(10.0),
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'Error Message',
+                      textAlign: TextAlign.center,
+                      style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                            color: Colors.white,
+                          ),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                ElevatedButton(
-                  onPressed: () {
-                    // TODO: Show Loading Indicator
-                    try {
-                      FirebaseAuth.instance
-                          .createUserWithEmailAndPassword(
-                        email: _emailController.text,
-                        password: _passwordController.text,
-                      )
-                          .then((value) {
-                        FireStoreManager.addUserData(
-                                value.user!.uid,
-                                _nameController.text,
-                                _emailController.text,
-                                _phoneController.text)
-                            .then(
-                          (value) {
-                            FirebaseAnalytics.instance
-                                .logLogin(loginMethod: 'email');
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {
+                      context.loaderOverlay.show();
+                      try {
+                        FirebaseAuth.instance
+                            .createUserWithEmailAndPassword(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        )
+                            .then((value) {
+                          FireStoreManager.addUserData(
+                                  value.user!.uid,
+                                  _nameController.text,
+                                  _emailController.text,
+                                  _phoneController.text)
+                              .then(
+                            (value) {
+                              FirebaseAnalytics.instance
+                                  .logLogin(loginMethod: 'email');
 
-                            Navigator.pushAndRemoveUntil(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => const LoadingScreen(),
-                                settings:
-                                    const RouteSettings(name: 'LoadingScreen'),
-                              ),
-                              (route) => false,
-                            );
-                          },
-                        );
-                      });
-                    } on FirebaseAuthException catch (exception) {
-                      // TODO: Handle Exception through Error Message
-                      print(exception);
-                    }
-                  },
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(100, 75),
-                    backgroundColor: const Color(0xFF6C63FF),
+                              Navigator.pushAndRemoveUntil(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const LoadingScreen(),
+                                  settings: const RouteSettings(
+                                      name: 'LoadingScreen'),
+                                ),
+                                (route) => false,
+                              ).then(
+                                (value) => context.loaderOverlay.hide(),
+                              );
+                            },
+                          );
+                        });
+                      } on FirebaseAuthException catch (exception) {
+                        context.loaderOverlay.hide();
+                        // TODO: Handle Exception through Error Message
+                        print(exception);
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(100, 75),
+                      backgroundColor: const Color(0xFF6C63FF),
+                    ),
+                    child: const Text('Register'),
                   ),
-                  child: const Text('Register'),
-                ),
-              ],
-            ),
-          ],
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
